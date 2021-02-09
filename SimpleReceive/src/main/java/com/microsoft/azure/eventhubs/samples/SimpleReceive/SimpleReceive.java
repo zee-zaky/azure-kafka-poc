6f4a2c650064e69146013a83d205b2f3973d1880
zee-zaky <60630740+zee-zaky@users.noreply.github.com>
2021-02-10 08:16:17 +1300

    Add files via upload

--- SimpleReceive/src/main/java/com/microsoft/azure/eventhubs/samples/SimpleReceive/SimpleReceive.java
+++ SimpleReceive/src/main/java/com/microsoft/azure/eventhubs/samples/SimpleReceive/SimpleReceive.java
@@ -13,36 +13,40 @@ import com.azure.messaging.eventhubs.models.ErrorContext;
 import com.azure.messaging.eventhubs.models.EventContext;
 import com.azure.storage.blob.BlobContainerAsyncClient;
 import com.azure.storage.blob.BlobContainerClientBuilder;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.config.SaslConfigs;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.function.Consumer;
 
-import org.apache.kafka.clients.producer.*;
+/*import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.LongSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.apache.kafka.common.config.SaslConfigs;
+import org.apache.kafka.common.config.SaslConfigs;*/
 
 import java.util.Properties;
 
 public class SimpleReceive {
-	private static final String EH_NAMESPACE_CONNECTION_STRING = "Endpoint=sb://ehubns-kafkapoc-zaky.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=8IEM/bAExPyMKi1ItJA+SIe+UAZpbcO5B5Jz5ztCtW4=";
-	private static final String eventHubName = "ehub-kafkapoc-zaky";
-	private static final String STORAGE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=storagekafkapoczaky;AccountKey=6vhwa60hiPYNWvwbrO9EslEvgez+DtDO8dYmFtetlLd6vyAz1AEHC8mFCK9HNI5ujOj2ETE0mzyYCgGkEIQZHg==";
+	private static final String EH_NAMESPACE_CONNECTION_STRING = "Endpoint=sb://kafkatestin.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=tQp4AG95dXZ2vFPkuwJg9ca/AaqPNMSgU8qIBpuDsEA=";
+	private static final String eventHubName = "testeventhub";
+	private static final String STORAGE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=azurekafkapoc;AccountKey=8kv1I5Y/jGG6+YGO9lVO3CakOMugKabLLrImXsBSYEIRZQ6xOzTrPeckR9rGeg4upSegnjsUTQhUUJHKEDTZkQ==";
 	private static final String STORAGE_CONTAINER_NAME = "messages";
 
 	// Create the Properties class to instantiate the Consumer with the desired settings:
-	private static Properties Kafka_Props = new Properties();
+	/*private static Properties Kafka_Props = new Properties();
 	private static final String Kafka_Topic = "test";
-	private static KafkaProducer<String, String> Kafka_Producer = null;
+	private static KafkaProducer<String, String> Kafka_Producer = null;*/
 
 
 	public static final Consumer<EventContext> PARTITION_PROCESSOR = eventContext -> {
 		System.out.printf("Processing event from partition %s with sequence number %d with body: %s %n",
 				eventContext.getPartitionContext().getPartitionId(), eventContext.getEventData().getSequenceNumber(), eventContext.getEventData().getBodyAsString());
 
-		Kafka_Producer.send(new ProducerRecord<String, String>(Kafka_Topic, eventContext.getEventData().getBodyAsString()));
+		/*Kafka_Producer.send(new ProducerRecord<String, String>(Kafka_Topic, eventContext.getEventData().getBodyAsString()));*/
 
 		if (eventContext.getEventData().getSequenceNumber() % 10 == 0) {
 			eventContext.updateCheckpoint();
@@ -61,18 +65,22 @@ public class SimpleReceive {
 				.containerName(STORAGE_CONTAINER_NAME)
 				.buildAsyncClient();
 
+		/*System.setProperty("http.proxyHost","nz-proxy01.westpac.co.nz");
+		System.setProperty("http.proxyPort", "8080");*/
+
 		EventProcessorClientBuilder eventProcessorClientBuilder = new EventProcessorClientBuilder()
 				.connectionString(EH_NAMESPACE_CONNECTION_STRING, eventHubName)
 				.consumerGroup(EventHubClientBuilder.DEFAULT_CONSUMER_GROUP_NAME)
 				.processEvent(PARTITION_PROCESSOR)
 				.processError(ERROR_HANDLER)
 				.transportType(AmqpTransportType.AMQP_WEB_SOCKETS) //configure EventHubClient transport to AmqpWebSockets
+				//.proxyOptions("http://bccluster.inet.westpac.co.nz:80/")
 				.checkpointStore(new BlobCheckpointStore(blobContainerAsyncClient));
 
 		EventProcessorClient eventProcessorClient = eventProcessorClientBuilder.buildEventProcessorClient();
 
 		// Create the Kafka Properties class to instantiate the Consumer with the desired settings:
-		String jaasTemplate = "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"%s\" password=\"%s\";";
+		/*String jaasTemplate = "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"%s\" password=\"%s\";";
 		String jaasCfg = String.format(jaasTemplate, "kafka", "bpXalEi1iGwg");
 
 		Kafka_Props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "52.163.204.3:9092");
@@ -83,7 +91,7 @@ public class SimpleReceive {
 		Kafka_Props.put(SaslConfigs.SASL_MECHANISM,"PLAIN");
 
 
-		Kafka_Producer = new KafkaProducer<String, String>(Kafka_Props);
+		Kafka_Producer = new KafkaProducer<String, String>(Kafka_Props);*/
 
 
 		System.out.println("Starting event processor");
